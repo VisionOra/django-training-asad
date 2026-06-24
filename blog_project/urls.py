77106e1ad33681f966581067d2169d9b6bfe0b23
Your Name <muhammad.asad@artilence.com>
2026-06-24 16:06:03 +0500

    Refactor project structure by moving blog app into apps folder

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('blog.urls')),
+
+    # blog app urls
+    path('', include('apps.blog.urls')),
+
+    # JWT token urls
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Swagger / OpenAPI
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
